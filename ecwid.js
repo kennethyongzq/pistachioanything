cc4db90cf4b4ee59ff0d2895e0fa7cc220c3eded
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-13 21:46:16 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,19 +1,1 @@
-/*
-// Initialize extra fields
-ec = ec || {};
-ec.order = ec.order || {};
-ec.order.extraFields = ec.order.extraFields || {};
-
-// Add a new optional text input 'How should we sign the package?' to shipping address form
-ec.order.extraFields.additional_remarks = {
-    'title': 'Additional Remarks',
-    'textPlaceholder': 'Additional remarks',
-    'type': 'text',
-    'tip': '',
-    'required': false,
-    'checkoutDisplaySection': 'shipping_address'
-};
-
-Ecwid.refreshConfig();
-*/
 console.log("Helloworld Chris");
