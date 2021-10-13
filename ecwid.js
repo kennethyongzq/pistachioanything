d595e62af9c58dcb434a105e2371bfacc4bd7169
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-13 22:45:57 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,22 +1,18 @@
-console.log("Version 4 : try ACCESSING ARRAY KEY");
+console.log("Version 5 : {}");
 var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
 console.log(data);
 
 ec = ec || {};
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {}; 
+ec.order.extraFields.ecwid_order_delivery_time_interval_start = ec.order.extraFields.ecwid_order_delivery_time_interval_start || {}; 
+ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides = ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides || {}; 
 
 console.log("extraFields");
 console.log(ec.order.extraFields);
-var overrides_array = ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides;
-console.log("overrides_array");
-console.log(overrides_array);
 
-console.log("ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[0]");
-console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[0]);
-
-for (let i = 0; i < overrides_array.length; i++) {
-  var prev_options = overrides_array[i].fieldsToOverride.datePickerOptions 
+for (let i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
+  var prev_options = overrides_array[i].fieldsToOverride.datePickerOptions;
 }
 console.log("prev_options");
 console.log(prev_options);
