12fa5b84cd731cae7065fb3ad57d86c17d0033ef
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-13 22:16:13 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -4,5 +4,14 @@ console.log(data);
 
 ec = ec || {};
 ec.order = ec.order || {};
-console.log("ec_order");
-console.log(ec.order);
+ec.order.extraFields = ec.order.extraFields || {}; 
+
+console.log("extraFields");
+console.log(ec.order.extraFields);
+for (let i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
+  var prev_options = ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[i].fieldsToOverride.datePickerOptions 
+}
+console.log("prev_options");
+console.log(prev_options);
+
+
