bfd512d48cc848ced8014e74b27870259ca923c4
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-13 23:08:07 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,6 +1,8 @@
 console.log("Version 6 : {}");
 var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
 console.log(data);
+data = data.toJSON();
+console.log(data);
 
 ec = ec || {};
 ec.order = ec.order || {};
@@ -8,11 +10,25 @@ ec.order.extraFields = ec.order.extraFields || {};
 ec.order.extraFields.ecwid_order_delivery_time_interval_start = ec.order.extraFields.ecwid_order_delivery_time_interval_start || {};
 console.log("extraFields");
 console.log(ec.order.extraFields);
-
-for (let i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
-  var prev_options = ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[i].fieldsToOverride.datePickerOptions;
+function my_check(in_date) {
+    for (let j = 0; j < data.length; j++) {
+        var blockout_date = new Date();
+        blockout_date.setTime(data[j]);
+        if (blockout_date.getDate() == in_date.getDate() && blockout_date.getMonth() == in_date.getMonth() && blockout_date.getFullYear() == in_date.getFullYear()){
+            // in_date is blockout_date
+            console.log("found a blockout date");
+            return [false, "", ""];
+        }
+    }
+    return [true, "", ""];
 }
-console.log("prev_options");
-console.log(prev_options);
+for (let i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
+    var prev_options = ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[i].fieldsToOverride.datePickerOptions;    
+    console.log("prev_options");
+    console.log(prev_options);
+    ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[i].fieldsToOverride.datePickerOptions.beforeShowDay = my_check;
+}
 
+
+Ecwid.refreshConfig();
 
