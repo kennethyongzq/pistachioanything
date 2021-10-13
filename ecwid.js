c7018cde1d9a1ca4ca9545775401532cca066a76
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-13 21:55:11 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,1 +1,8 @@
-console.log("Helloworld Chris");
+console.log("Version 1");
+var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
+console.log(data);
+
+ec = ec || {};
+ec.order = ec.order || {};
+console.log("ec_order");
+console.log(ec.order);
