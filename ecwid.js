8538a07dbaa1baa4417861971c818e909a0065f2
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-14 14:21:32 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,6 +1,6 @@
 function update_blockout_dates(){
     console.log("update_blockout_dates");
-    console.log("Version 17 : update_blockout_dates also during page loaded");
+    console.log("Version 1 : 14 OCT 2021");
     var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
     console.log(data);
     data = JSON.parse(data)
@@ -15,39 +15,11 @@ function update_blockout_dates(){
     console.log("extraFields");
     console.log(ec.order.extraFields);
 
-    /*
-    console.log("orderDetailsDisplaySection");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.orderDetailsDisplaySection);
-    console.log("overrides");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides);
-    console.log("overrides,length");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length);
-    console.log("overrides0,fields to override");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[0].fieldsToOverride);
-    console.log("overrides0,fields to override,datePickerOptions");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[0].fieldsToOverride.datePickerOptions);
-    console.log("overrides0,fields to override,datePickerOptions,disallowDates");
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[1].fieldsToOverride.datePickerOptions.disallowDates);
-    var array_to_append = new Array('2021-12-04 00:00:00', '2021-12-04 23:59:59');
-    console.log("array_to_append");
-    console.log(array_to_append);
-
-    console.log("overrides0,fields to override,datePickerOptions,disallowDates, after push");
-    ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[1].fieldsToOverride.datePickerOptions.disallowDates.push(array_to_append);
-    console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[1].fieldsToOverride.datePickerOptions.disallowDates);
-    */
-
-
-
     for (var i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
         for (var j = 0; j < data.length; j++) {
             var blockout_date = new Date();
             blockout_date.setTime(data[j]);
-            console.log("adding blockout_date")
-            console.log(blockout_date)
             var array_to_append = new Array(blockout_date.getFullYear() + "-" + String(blockout_date.getMonth()+1).padStart(2, '0') + "-" + String(blockout_date.getDate()).padStart(2, '0') + " 00:00:00", blockout_date.getFullYear() + "-" + String(blockout_date.getMonth()+1).padStart(2, '0') + "-" + String(blockout_date.getDate()).padStart(2, '0') + " 23:59:59");
-            console.log("array_to_append");
-            console.log(array_to_append);
             ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[i].fieldsToOverride.datePickerOptions.disallowDates.push(array_to_append);
         }
     }
@@ -55,11 +27,7 @@ function update_blockout_dates(){
         for (var j = 0; j < data.length; j++) {
             var blockout_date = new Date();
             blockout_date.setTime(data[j]);
-            console.log("adding blockout_date")
-            console.log(blockout_date)
             var array_to_append = new Array(blockout_date.getFullYear() + "-" + String(blockout_date.getMonth()+1).padStart(2, '0') + "-" + String(blockout_date.getDate()).padStart(2, '0') + " 00:00:00", blockout_date.getFullYear() + "-" + String(blockout_date.getMonth()+1).padStart(2, '0') + "-" + String(blockout_date.getDate()).padStart(2, '0') + " 23:59:59");
-            console.log("array_to_append");
-            console.log(array_to_append);
             ec.order.extraFields.ecwid_order_pickup_time.overrides[i].fieldsToOverride.datePickerOptions.disallowDates.push(array_to_append);
         }
     }
