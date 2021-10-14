a5649c38daa4d3ee468790668ab7541e5848cab4
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-10-14 10:33:59 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,7 +1,7 @@
 Ecwid.OnPageLoaded.add(function(page) {
     console.log("OnPageLoaded");
     if (page.type == "CHECKOUT_DELIVERY") {
-        console.log("Version 14 : the next day4 : Ecwid.refreshConfig() check if before show day is defined;");
+        console.log("Version 14 : array_to_append");
         var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
         console.log(data);
         data = JSON.parse(data)
@@ -29,8 +29,10 @@ Ecwid.OnPageLoaded.add(function(page) {
         console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[0].fieldsToOverride.datePickerOptions);
         console.log("overrides0,fields to override,datePickerOptions,disallowDates");
         console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[1].fieldsToOverride.datePickerOptions.disallowDates);
-        console.log("overrides0,fields to override,datePickerOptions,beforeShowDay");
-        console.log(ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides[1].fieldsToOverride.datePickerOptions.beforeShowDay);
+        var array_to_append = new Array('2021-12-04 00:00:00', '2021-12-04 23:59:59');
+        
+        console.log("array_to_append");
+        console.log(array_to_append);
         /*
         for (var i = 0; i < ec.order.extraFields.ecwid_order_delivery_time_interval_start.overrides.length; i++) {
             console.log("2");
