5b1611406a615fa3ea1eca476eec0c6ce7d0efdc
kennethyongzq <89622896+kennethyongzq@users.noreply.github.com>
2021-11-23 13:07:44 +0800

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,12 +1,31 @@
-function update_blockout_dates(){
+function update_blockout_dates(cart_dict){
     console.log("update_blockout_dates");
-    console.log("Version 3.2");
+    console.log("Version 3.4");
     var data = Ecwid.getAppPublicConfig('custom-app-33883008-3');
     console.log(data);
     data = JSON.parse(data)
     data = data['blockout_dates'];
     console.log(data);
 
+    // Processing cart products to see if need to block out any epochs
+    var inv_data = data.inv;
+    var list_of_epochs = [];
+    Object.keys(cart_dict).forEach(function(key) {
+        console.log(key, cart_dict[key]);
+        var c_pid = key;
+        var c_qty = cart_dict[key];
+        for (var j = 0 ; j < inv_data.length ; j++){
+            var i_epoch = inv_data[j].epoch;
+            var i_pid = inv_data[j].pid;
+            var i_qty = inv_data[j].qty;
+
+            if (i_pid == c_pid){
+                console.log("There exist an item in cart that has inventory data");
+                console.log(i_pid);
+            }
+        }
+    });
+    
     ec = ec || {};
     ec.order = ec.order || {};
     ec.order.extraFields = ec.order.extraFields || {};
@@ -59,7 +78,7 @@ function convertCartToPidQtyDict(cart){
 Ecwid.OnCartChanged.add(function(cart){
     console.log("OnCartChanged");
     var dict = convertCartToPidQtyDict(cart);
-    update_blockout_dates();
+    update_blockout_dates(dict);
 });
 
 Ecwid.OnPageLoad.add(function(page) {
@@ -71,7 +90,7 @@ Ecwid.OnPageLoaded.add(function(page) {
     console.log("OnPageLoaded");
     Ecwid.Cart.get(function(cart){
         var dict = convertCartToPidQtyDict(cart);
-        update_blockout_dates();    
+        update_blockout_dates(dict);    
     });
 
     
